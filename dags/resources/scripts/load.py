0386128cf4f622e7b2004b0737a7012f8475f82a
malikfajarh <malikfajarhidayatullah@gmail.com>
2025-02-19 14:55:38 +0700

    improve data load time

--- dags/resources/scripts/load.py
+++ dags/resources/scripts/load.py
@@ -1,26 +1,40 @@
 def load_table(table, **kwargs):
-    import pandas as pd
     from airflow.providers.postgres.hooks.postgres import PostgresHook
-    from sqlalchemy import text
+    import gzip
 
     data_interval_start = kwargs['data_interval_start']
     data_interval_end   = kwargs['data_interval_end']
     ingestion_mode      = kwargs["params"][table]
 
-    df = pd.read_parquet(f"data/ecommerce/bronze/{table}/{data_interval_start}_{data_interval_end}_{ingestion_mode}.parquet")
+    csv_path = f"data/ecommerce/{table}/{data_interval_start}_{data_interval_end}_{ingestion_mode}.csv"
 
-    engine = PostgresHook("postgres_dw").get_sqlalchemy_engine()
-    with engine.connect() as conn:
-        conn = conn.execution_options(isolation_level="AUTOCOMMIT")
-        conn.execute(text("CREATE SCHEMA IF NOT EXISTS bronze;"))
+    postgres_hook = PostgresHook(postgres_conn_id="postgres_dw")
+    conn = postgres_hook.get_conn() 
+    cursor = conn.cursor()
+
+    if ingestion_mode == "full" and table == "product":
+        query = """
+            DO $$ 
+            DECLARE 
+                tbl RECORD;
+            BEGIN
+                FOR tbl IN (SELECT table_name FROM information_schema.tables 
+                            WHERE table_schema = 'bronze' 
+                            AND table_name IN ('transaction', 'product', 'customer'))
+                LOOP
+                    EXECUTE FORMAT('TRUNCATE TABLE bronze.%I CASCADE', tbl.table_name);
+                END LOOP;
+            END $$;
+        """
+        cursor.execute(query)
         
-        if ingestion_mode == "incremental":
-            mode = "append"
-        else:
-            mode = "replace"
+    with gzip.open(csv_path, 'rt') as f:  
+        cursor.copy_expert(f"COPY bronze.{table} FROM STDIN WITH CSV HEADER", f)
 
-        df.to_sql(table, conn, schema="bronze", index=False, if_exists=mode, method="multi", chunksize=50000)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print(f'{table} data successfully loaded to PostgreSQL with {ingestion_mode} mode')
 
-    print(f'{table} data successfully loaded to postgresql')
 
 
